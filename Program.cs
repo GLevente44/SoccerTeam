abbcbf29944e7b06e12ee377eea806d78068c717
Levente <gecsel@kkszki.hu>
2024-11-22 11:35:30 +0100

    SoccerTeam project is created

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerTeam
+{
+    internal class Program
+    {
+        public static void SoccerTeam()
+        {
+
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            SoccerTeam();
+        }
+    }
+}
